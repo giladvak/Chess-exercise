a1e2392f9ed1987f10fc5acd57337c5eb6f15adf
giladvak <102975677+giladvak@users.noreply.github.com>
2022-04-17 22:10:03 +0300

    Updated files

--- chess.js
+++ chess.js
@@ -1,32 +1,52 @@
 
-window.addEventListener('load',()=>{
-//create table element
 
-const table=document.createElement('table');
-//apending table
-document.body.appendChild(table);
-
-table.classList.add ('board');
-
-//loop
-for (let i = 0; i <8 ; i++) {
-    const loopOne=document.createElement('tr');
-    table.appendChild(loopOne);
-   {
+window.addEventListener('load', () => {
+    let typeBlack='black'
+    let typeWhite='white'
+      
+    function createBoard(){
+            const div = document.createElement('div');
+             document.body.appendChild(div);
+             div.className='outerBox'
+        const table = document.createElement('table');
+        div.appendChild(table);
+        table.classList.add('board');
+       
+        for (let i = 0; i < 8; i++) {
+            const row = document.createElement('tr');
+            table.appendChild(row);
+            
         
-    }
-   for (let j = 0;j < 8; j++) {
-       let td=document.createElement('td')
-       loopOne.appendChild(td)
-       if (j%2===i%2){
-        td.classList.add ('white')
-        
-       }else {
-       td.classList.add ('black');}
-   }
-   
-}
-})
+            for (let j = 0; j < 8; j++) {
+            const cell=document.createElement('td');
+            row.appendChild(cell)
+                if (j % 2 === i % 2) {
+                    cell.classList.add('white')
+                    
+    
+                } else {
+                    cell.classList.add('black');
+                }
+          }
+        }
+        // const img =  document.createElement('img');
+        // img.src = 'pawns/black/Chess_bdt45.svg';
+        // img.classList.add('tool')
+        // const cell = document.querySelector('td')
+        // cell.appendChild(img)
+    };
+
+
+    // Main game flow
+    createBoard();
+    addImagesToBoard();
+    clickCell();
+
+});
+
+
+
+
 
 
 
