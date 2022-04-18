81f2f888d825e9486b1a2b84535fcd120aaa3f45
giladvak <102975677+giladvak@users.noreply.github.com>
2022-04-18 22:18:45 +0300

    added class

--- chess.js
+++ chess.js
@@ -1,6 +1,55 @@
 let typeBlack='black'
     let typeWhite='white'
     let selectedCell;
+    let pieces=[];
+    let pieces1=[];
+
+
+
+
+
+    class Piece { 
+        constructor(row,col,type,player){
+            this.row=row
+            this.col=col
+            this.type=type
+            this.player=player
+        }
+        
+      }
+      function getInitialBoard(){
+          let result=[];
+         result.push(new Piece(0,0, 'rook', typeWhite))
+         result.push(new Piece(0,1, 'knight',typeWhite))
+         result.push(new Piece(0,2, 'bishop', typeWhite))
+         result.push(new Piece(0,3, 'king',typeWhite))
+         result.push(new Piece(0,4, 'queen', typeWhite))
+         result.push(new Piece(0,5, 'bishop',typeWhite))
+         result.push(new Piece(0,6, 'knight', typeWhite))
+         result.push(new Piece(0,7, 'rook',typeWhite))
+         for (let i = 0; i < 8; i++) {
+            result.push(new Piece(1,i,'pawn',typeWhite))
+             
+         }
+          return result;
+          
+      }
+      function getInitialBoardBlack(){
+        let result=[];
+       result.push(new Piece(7,0, 'rook', typeBlack))
+       result.push(new Piece(7,1, 'knight',typeBlack))
+       result.push(new Piece(7,2, 'bishop', typeBlack))
+       result.push(new Piece(7,3, 'king',typeBlack))
+       result.push(new Piece(7,4, 'queen', typeBlack))
+       result.push(new Piece(7,5, 'bishop',typeBlack))
+       result.push(new Piece(7,6, 'knight', typeBlack))
+       result.push(new Piece(7,7, 'rook',typeBlack))
+       for (let i = 0; i < 8; i++) {
+          result.push(new Piece(6,i,'pawn',typeBlack))
+           
+       }
+        return result;
+    }
     function getImg(cell,type,name){
         const img =  document.createElement('img');
         img.src = 'pawns/' + type + '/' + name + '.svg'
@@ -23,6 +72,7 @@ let typeBlack='black'
    
       
     function createBoard(){
+        
             const div = document.createElement('div');
              document.body.appendChild(div);
              div.className='outerBox'
@@ -38,6 +88,7 @@ let typeBlack='black'
             for (let j = 0; j < 8; j++) {
             const cell=document.createElement('td');
             row.appendChild(cell)
+            cell.id=(i.toString() + ',' +j.toString())
                 if (j % 2 === i % 2) {
                     cell.classList.add('white')
                     
@@ -47,15 +98,17 @@ let typeBlack='black'
 
                 } 
                
-                if (i===0){
-                addImageByIndex(cell,typeWhite,j)
-                } else if (i===7){
-                 addImageByIndex(cell,typeBlack,j)
-                }
-                if (i===1){
-                    getImg(cell,typeWhite,'pawn')
-                }else if (i===6){
-                    getImg(cell,typeBlack,'pawn')}
+                // if (i===0){
+                // addImageByIndex(cell,typeWhite,j)
+                // } else if (i===7){
+                //  addImageByIndex(cell,typeBlack,j)
+                // }
+                // if (i===1){
+                //     getImg(cell,typeWhite,'pawn')
+                // }else if (i===6){
+                //     getImg(cell,typeBlack,'pawn')}
+
+               
                     function removeColor(){
                         
                     } 
@@ -70,24 +123,24 @@ let typeBlack='black'
                         cell.classList.add('selected')
                     }
                     cell.addEventListener('click',changeEventColor);
-                    
+                   
+                   
             }
               
             }                                                                                               
-                                                                                                           
-                 
+                pieces=getInitialBoard();
+                pieces1=getInitialBoardBlack();
+                blackOrWhite(pieces)
+                blackOrWhite(pieces1)
+                function blackOrWhite(arr){
+                for(let piece of arr){
+                    getImg(table.rows[piece.row].cells[piece.col],piece.player,piece.type);
+                }
+            }
+              
                
           }
-          class Tool { 
-            constructor(row,col,type,player){
-                this.row=row
-                this.col=col
-                this.type=type
-                this.player=player
-            }
-            
-
-          }
+         
         
         
        
