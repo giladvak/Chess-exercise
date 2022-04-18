227b571bc5279ef954c7d4e006235971fc9937c3
giladvak <102975677+giladvak@users.noreply.github.com>
2022-04-18 19:59:16 +0300

    tools

--- chess.js
+++ chess.js
@@ -1,8 +1,26 @@
-
-
-window.addEventListener('load', () => {
-    let typeBlack='black'
+let typeBlack='black'
     let typeWhite='white'
+    let selectedCell;
+    function getImg(cell,type,name){
+        const img =  document.createElement('img');
+        img.src = 'pawns/' + type + '/' + name + '.svg'
+        img.classList.add('tool')
+        cell.appendChild(img)
+    }
+    function addImageByIndex(cell,type,j){
+        if (j===0||j===7) {
+            getImg(cell,type,'rook')   
+        } else if (j===1||j===6){
+            getImg(cell,type,'knight')
+        }else if (j===2||j===5){
+            getImg(cell,type,'bishop')
+        }else if(j===3){
+            getImg(cell,type,'king')
+        }else if(j===4){
+            getImg(cell,type,'queen')
+        }
+    }
+   
       
     function createBoard(){
             const div = document.createElement('div');
@@ -26,30 +44,70 @@ window.addEventListener('load', () => {
     
                 } else {
                     cell.classList.add('black');
+
+                } 
+               
+                if (i===0){
+                addImageByIndex(cell,typeWhite,j)
+                } else if (i===7){
+                 addImageByIndex(cell,typeBlack,j)
                 }
+                if (i===1){
+                    getImg(cell,typeWhite,'pawn')
+                }else if (i===6){
+                    getImg(cell,typeBlack,'pawn')}
+                    function removeColor(){
+                        
+                    } 
+                    function changeEventColor(e){
+                        console.log(e.currentTarget);
+                        
+                        if(selectedCell!==undefined){
+                        selectedCell.classList.remove('selected')
+                        }
+                      
+                        selectedCell=e.currentTarget;
+                        cell.classList.add('selected')
+                    }
+                    cell.addEventListener('click',changeEventColor);
+                    
+            }
+              
+            }                                                                                               
+                                                                                                           
+                 
+               
           }
-        }
-        // const img =  document.createElement('img');
-        // img.src = 'pawns/black/Chess_bdt45.svg';
-        // img.classList.add('tool')
-        // const cell = document.querySelector('td')
-        // cell.appendChild(img)
-    };
+          class Tool { 
+            constructor(row,col,type,player){
+                this.row=row
+                this.col=col
+                this.type=type
+                this.player=player
+            }
+            
+
+          }
+        
+        
+       
+   
+    
+
+    
+   
+    window.addEventListener('load', createBoard);
+   
+   
+   
+   
+   
+    
+  
 
 
-    // Main game flow
-    createBoard();
-    addImagesToBoard();
-    clickCell();
-
-});
+   
+    
 
 
-
-
-
-
-
-
-
-
+  
